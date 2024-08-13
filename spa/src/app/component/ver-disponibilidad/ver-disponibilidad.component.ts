eaf30f2eb677677fccf1ad20400a25342b9bc45b
GodLeve1 <piurijoel2002@gmail.com>
2024-08-12 22:18:29 -0500

    Cambio reservacion

--- spa/src/app/component/ver-disponibilidad/ver-disponibilidad.component.ts
+++ spa/src/app/component/ver-disponibilidad/ver-disponibilidad.component.ts
@@ -10,16 +10,40 @@ import { ReservationService } from '../../services/reservation.service';
 export class VerDisponibilidadComponent {
   disponibilidadForm: FormGroup;
   disponible: boolean | null = null;
+  availableTimes: string[] = [];
 
   constructor(private fb: FormBuilder, private reservationService: ReservationService) {
     this.disponibilidadForm = this.fb.group({
       date: [''],
       time: ['']
     });
+
+    // Genera las horas disponibles
+    this.generateAvailableTimes();
+  }
+
+  generateAvailableTimes() {
+    const times = [];
+    const intervals = ['09:00', '09:30', '10:00', '10:30', '11:00', '11:30', '12:00',
+                       '14:00', '14:30', '15:00', '15:30', '16:00', '16:30', '17:00'];
+    for (const interval of intervals) {
+      times.push(interval);
+    }
+    this.availableTimes = times;
   }
 
   verificarDisponibilidad() {
     const { date, time } = this.disponibilidadForm.value;
+    const selectedDateTime = new Date(`${date}T${time}`);
+    const currentDateTime = new Date();
+
+    // Validación: no permitir reservar fechas y horas anteriores al momento actual
+    if (selectedDateTime < currentDateTime) {
+      this.disponible = false;
+      console.warn('No se puede reservar una fecha u hora anterior al momento actual.');
+      return;
+    }
+
     this.reservationService.verificarDisponibilidad(date, time).subscribe(
       (response: { disponible: boolean }) => {
         this.disponible = response.disponible;
